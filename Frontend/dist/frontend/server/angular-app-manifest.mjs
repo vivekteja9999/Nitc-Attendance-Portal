345f0d8159561df75a53ee502139b53ca69500c7
Teja5353 <tejaniduram@gmail.com>
2025-03-05 23:26:50 +0530

    Corrected bugs

--- Frontend/dist/frontend/server/angular-app-manifest.mjs
+++ Frontend/dist/frontend/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 717, hash: '702ab1abcd2bfd728dc0cb0e8f511022e913280a2c35b5287a555b0f9ad668aa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1230, hash: 'cce3ff744b3a1d73219ca644e9175b0467177ab85e6df920119d0b3d4dd618df', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'user-dashboard/index.html': {size: 5827, hash: '47e9c8e7d9021b4f1e4f2b3a2dfd7915538738cb862d36d32c60e90345d49f0a', text: () => import('./assets-chunks/user-dashboard_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 5827, hash: '47e9c8e7d9021b4f1e4f2b3a2dfd7915538738cb862d36d32c60e90345d49f0a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 717, hash: 'c849c5ad402e133c942af132a04881d069e86f77ce33b420d7847ae708d9c056', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1230, hash: '3005b5dc67bd994eea2626717d6451caf2fb2bdd328c91579e2e952b3bd2f90f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 6486, hash: '77a2a8d38d4f3f62bfaa8464d1e36cab62a8cae54651eb52cdcf8c9c4c33a531', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'user-dashboard/index.html': {size: 2927, hash: '152cb020a28e20b09fc454d53b35420e25b8877ea0bea549cec129146dbd735d', text: () => import('./assets-chunks/user-dashboard_index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
--- Frontend/dist/frontend/server/assets-chunks/index_csr_html.mjs
+++ Frontend/dist/frontend/server/assets-chunks/index_csr_html.mjs
@@ -10,6 +10,6 @@ export default `<!doctype html>
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-DZLB327W.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-Y3RTXW7I.js" type="module"></script></body>
 </html>
 `;
