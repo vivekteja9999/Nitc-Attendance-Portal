22f42bd206cc1ad4c8e54e78e36a87015455fbda
vivekteja9999 <vivekteja9999@gmail.com>
2025-03-20 02:36:12 +0530

    Cycle Borrow Implementation

---
+++ Frontend/src/app/cycle/borrow/borrow.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-borrow',
+  imports: [],
+  templateUrl: './borrow.component.html',
+  styleUrl: './borrow.component.css'
+})
+export class BorrowComponent {
+
+}
